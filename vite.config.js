b7cd5c36b7ae5d2564cbb4cac806336f9f8cf0b5
0x5448594f <derive.theo@proton.me>
2026-06-29 16:42:04 +0200

    Fix: Add posts files to link

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import fs from "fs";
+
+const input = {
+  index: resolve(__dirname, "index.html"),
+  about: resolve(__dirname, "about.html"),
+  posts: resolve(__dirname, "posts.html"),
+  add_post: resolve(__dirname, "add_post.html"),
+};
+
+const postsDir = resolve(__dirname, "posts");
+
+if (fs.existsSync(postsDir)) {
+  const files = fs.readdirSync(postsDir);
+
+  files
+    .filter(file => file.endsWith(".html"))
+    .forEach(file => {
+      input[`posts/${file.replace(".html", "")}`] = resolve(postsDir, file);
+    });
+}
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about.html"),
-        posts: resolve(__dirname, "posts.html"),
-        add_post: resolve(__dirname, "add_post.html"),
-      },
+      input,
     },
   },
 });
